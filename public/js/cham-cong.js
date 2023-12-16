d0849617b26d92ea51fe7e456376c6d68356ac3f
huy.ntq02 <huy.ntq02@gmail.com>
2023-12-16 11:47:47 +0700

    Update VSCode settings and view templates

--- public/js/cham-cong.js
+++ public/js/cham-cong.js
@@ -1,4 +1,3 @@
-
 let calendar;
 
 $(() => {
@@ -6,32 +5,60 @@ $(() => {
     const calendarEl = document.getElementById("calendar");
     calendar = new FullCalendar.Calendar(calendarEl, {
         initialView: "dayGridMonth",
-        titleFormat: 'DD/MM/YY',
-        themeSystem: 'bootstrap5',
+        titleFormat: "DD/MM/YY",
+        themeSystem: "bootstrap5",
         navLinks: true,
         navLinkDayClick: function (date, jsEvent) {
-            console.log('day', moment(date).format('YYYY-MM-DD'));
-            console.log('coords', jsEvent.pageX, jsEvent.pageY);
+            console.log("day", moment(date).format("YYYY-MM-DD"));
+            console.log("coords", jsEvent.pageX, jsEvent.pageY);
         },
         headerToolbar: {
-            left: 'prev,next today',
-            center: 'title',
-            right: 'dayGridMonth,timeGridWeek,timeGridDay'
+            left: "prev,next today",
+            center: "title",
+            right: "dayGridMonth,timeGridWeek,timeGridDay",
         },
         eventSources: [
             {
-                url: '/cham-cong/events',
-                color: 'yellow',
-                textColor: 'black'
+                url: "/cham-cong/events",
+                color: "yellow",
+                textColor: "black",
             },
         ],
         eventClick: function (info) {
             const ngayCongId = info.event.id;
 
-            $('#chinh-sua-ngay-cong-modal').modal('show');
-        }
+            $.ajax({
+                url: `/cham-cong/events/${ngayCongId}`,
+                method: "GET",
+                success: function (response) {
+                    const ngayCong = response.data;
+                    console.log("ngayCong: ", ngayCong);
+                    $(".chinh-sua-ngay-cong-btn").data("ngay-lam-id", ngayCong._id);
+
+                    $("#nguoi-lam-option-modal")
+                        .text(ngayCong.nguoiLam.fullName || ngayCong.nguoiLam.username)
+                        .val(ngayCong.nguoiLam._id);
+
+                    const gioBuoiSang = ngayCong.gioBuoiSang > 0 ? `+${ngayCong.gioBuoiSang}` : ngayCong.gioBuoiSang;
+                    const gioBuoiChieu =
+                        ngayCong.gioBuoiChieu > 0 ? `+${ngayCong.gioBuoiChieu}` : ngayCong.gioBuoiChieu;
+
+                    $(".gio-buoi-sang-input-modal").val(`${gioBuoiSang}`);
+                    $(".gio-buoi-chieu-input-modal").val(`${gioBuoiChieu}`);
+                    $(".gio-lam-them-input-modal").val(ngayCong.gioLamThem);
+
+                    const total = +ngayCong.gioBuoiSang + +ngayCong.gioBuoiChieu + +ngayCong.gioLamThem;
+                    $(".tong-gio-lam-them-modal").text(`${total} phút ~ ${(total / 180).toFixed(2)} buổi`);
+
+                    $("#chinh-sua-ngay-cong-modal").modal("show");
+                },
+                error: function (response) {
+                    Swal.fire(response.responseJSON?.message);
+                },
+            });
+        },
     });
-    calendar.setOption('locale', 'vi');
+    calendar.setOption("locale", "vi");
     calendar.render();
 
     $("input[name='daterange']").daterangepicker(
@@ -40,7 +67,7 @@ $(() => {
             startDate: moment(),
             endDate: moment(),
             locale: {
-                format: "MM/DD/YYYY",
+                format: "DD/MM/YYYY",
                 applyLabel: "Xác nhận",
                 cancelLabel: "Hủy",
                 daysOfWeek: ["CN", "T2", "T3", "T4", "T5", "T6", "T7"],
@@ -67,6 +94,18 @@ $(() => {
         },
     );
 
+    $(".gio-buoi-sang-input, .gio-buoi-chieu-input, .gio-lam-them-input").on("input", function () {
+        const isModal = $(this).attr("class").includes("modal");
+        const modal = isModal ? "-modal" : "";
+
+        const gioBuoiSang = $(`.gio-buoi-sang-input${modal}`).val();
+        const gioBuoiChieu = $(`.gio-buoi-chieu-input${modal}`).val();
+        const gioLamThem = $(`.gio-lam-them-input${modal}`).val();
+
+        const total = +gioBuoiSang + +gioBuoiChieu + +gioLamThem;
+        $(`.tong-gio-lam-them${modal}`).text(`${total} phút ~ ${(total / 180).toFixed(2)} buổi`);
+    });
+
     $(".them-du-lieu-btn").on("click", function () {
         const ngayLam = $(".ngay-lam-input").val();
         const gioBuoiSang = $(".gio-buoi-sang-input").val();
@@ -74,20 +113,54 @@ $(() => {
         const gioLamThem = $(".gio-lam-them-input").val();
         const nguoiLam = $(".nguoi-lam-input").val();
 
+        if (gioBuoiSang == 0 && gioBuoiChieu == 0 && gioLamThem == 0) {
+            Swal.fire("Vui lòng chọn chọn ít nhất một buổi");
+            return;
+        }
+
         const data = {
-            ngayLam: moment(ngayLam, "MM/DD/YYYY").format("DD/MM/YYYY"),
-            gioBuoiSang,
-            gioBuoiChieu,
-            gioLamThem,
+            ngayLam: moment(ngayLam, "DD/MM/YYYY").format("DD/MM/YYYY"),
+            gioBuoiSang: gioBuoiSang,
+            gioBuoiChieu: gioBuoiChieu,
+            gioLamThem: gioLamThem,
             nguoiLam,
         };
         console.log("Data: ", data);
+
         $.ajax({
             url: "/cham-cong",
             type: "POST",
             data: data,
             success: (response) => {
                 Swal.fire(response.message);
+                calendar.refetchEvents();
+            },
+            error: (response) => {
+                Swal.fire(response.responseJSON?.message);
+            },
+        });
+    });
+
+    $(document).on("click", ".chinh-sua-ngay-cong-btn", function () {
+        const ngayCongId = $(this).data("ngay-lam-id");
+        const gioBuoiSang = $(".gio-buoi-sang-input-modal").val();
+        const gioBuoiChieu = $(".gio-buoi-chieu-input-modal").val();
+        const gioLamThem = $(".gio-lam-them-input-modal").val();
+
+        const data = {
+            gioBuoiSang,
+            gioBuoiChieu,
+            gioLamThem,
+        };
+
+        $.ajax({
+            url: `/cham-cong/events/${ngayCongId}`,
+            type: "PUT",
+            data: data,
+            success: (response) => {
+                Swal.fire(response.message);
+                calendar.refetchEvents();
+                $("#chinh-sua-ngay-cong-modal").modal("hide");
             },
             error: (response) => {
                 Swal.fire(response.responseJSON?.message);
--- routers/cham-cong.js
+++ routers/cham-cong.js
@@ -1,5 +1,7 @@
 // create router file in nodejs
 
+const mongoose = require("mongoose");
+const ObjectId = mongoose.Types.ObjectId;
 const moment = require("moment");
 const XLSX = require("xlsx");
 
@@ -16,7 +18,11 @@ router.use(async (req, res, next) => {
 });
 
 router.get("", async (req, res) => {
-    const users = await User.find();
+    let users;
+    if (req.session.user.role === "admin") {
+        users = await User.find({ role: "internship" });
+        // console.log("users: ", users);
+    }
     return res.status(200).render("cham-cong", {
         users,
     });
@@ -30,10 +36,10 @@ router.post("", async (req, res) => {
         let { gioLamThem } = req.body;
         let { nguoiLam } = req.body;
 
+        let tongGio = +gioBuoiSang + +gioBuoiChieu + +gioLamThem;
+
         // nguoiLam = await User.findById(nguoiLam);
-        ngayLam = moment(ngayLam, 'DD/MM/YYYY');
-        console.log(`ngayLam: `, ngayLam);
-        console.log(`ngayLam: `, ngayLam.day());
+        ngayLam = moment(ngayLam, "DD/MM/YYYY");
 
         if (ngayLam.day() == 0) {
             return res.status(400).json({
@@ -62,6 +68,7 @@ router.post("", async (req, res) => {
             gioBuoiChieu,
             gioLamThem,
             nguoiLam,
+            tongGio,
         });
 
         await ngayLamModel.save();
@@ -76,28 +83,27 @@ router.post("", async (req, res) => {
     }
 });
 
-router.get('/events', async (req, res) => {
+router.get("/events", async (req, res) => {
     try {
         let { start, end } = req.query;
 
         if (!start || !end) {
-            const currentMonthStart = moment().startOf('month');
-            const currentMonthEnd = moment().endOf('month');
+            const currentMonthStart = moment().startOf("month");
+            const currentMonthEnd = moment().endOf("month");
             start = start || currentMonthStart;
             end = end || currentMonthEnd;
         }
-
 
         const events = await NgayLam.find({
             ngayLam: {
                 $gte: start,
                 $lte: end,
-            }
-        }).populate('nguoiLam');
+            },
+        }).populate("nguoiLam");
 
-        const formattedEvents = events.map(event => ({
+        const formattedEvents = events.map((event) => ({
             id: event._id,
-            title: `${event.nguoiLam?.fullName || event.nguoiLam.username}`,
+            title: `${event.nguoiLam?.fullName || event.nguoiLam?.username}`,
             start: moment(event.ngayLam).format(MOMENT_FORMAT),
             end: moment(event.ngayLam).format(MOMENT_FORMAT),
         }));
@@ -105,84 +111,176 @@ router.get('/events', async (req, res) => {
         res.status(200).json(formattedEvents);
     } catch (error) {
         console.error(error);
-        res.status(500).send('Internal Server Error');
+        res.status(500).send("Internal Server Error");
     }
 });
 
-router.get('/events/excel', async (req, res) => {
+router.get("/events/:id", async (req, res, next) => {
+    const id = req.params.id;
+    if (!ObjectId.isValid(id)) {
+        return next();
+    }
     try {
-        let { start, end } = req.query;
+        const event = await NgayLam.findById(id).populate("nguoiLam");
 
-        if (!start || !end) {
-            const currentMonthStart = moment().startOf('month');
-            const currentMonthEnd = moment().endOf('month');
-            start = start || currentMonthStart;
-            end = end || currentMonthEnd;
-        }
+        res.status(200).json({
+            data: event,
+            message: "Lấy dữ liệu thành công",
+        });
+    } catch (error) {
+        res.status(500).send(`Đã có lỗi xảy ra [code: ${error}]`);
+    }
+});
 
-        let events = await NgayLam.find({
-            ngayLam: {
-                $gte: start,
-                $lte: end,
-            }
-        }).populate('nguoiLam');
+router.put("/events/:id", async (req, res, next) => {
+    const id = req.params.id;
+    if (!ObjectId.isValid(id)) {
+        return next();
+    }
+    try {
+        let { gioBuoiSang } = req.body;
+        let { gioBuoiChieu } = req.body;
+        let { gioLamThem } = req.body;
 
-        let formattedEvents = events.map(event => {
-            // Destructure the event object and exclude _id and __v properties
-            const {
-                _id,
-                __v,
-                nguoiLam,
-                ...rest
-            } = event.toObject();
+        let tongGio = +gioBuoiSang + +gioBuoiChieu + +gioLamThem;
 
-            // Format the event and exclude _id and __v properties
+        const ngayLam = await NgayLam.findById(id);
+        ngayLam.gioBuoiSang = gioBuoiSang;
+        ngayLam.gioBuoiChieu = gioBuoiChieu;
+        ngayLam.gioLamThem = gioLamThem;
+        ngayLam.tongGio = tongGio;
+
+        await ngayLam.save();
+
+        return res.status(200).json({
+            message: "Cập nhập dữ liệu thành công",
+        });
+    } catch (error) {
+        return res.status(500).json({
+            message: `Lỗi từ hệ thống. Vui lòng thử lại sau. [code: ${error}]`,
+        });
+    }
+});
+
+router.get("/events/excel", async (req, res) => {
+    try {
+        const result = await NgayLam.aggregate([
+            {
+                $group: {
+                    _id: {
+                        $dateToString: {
+                            format: "%d",
+                            date: "$ngayLam",
+                        },
+                    },
+                    count: { $sum: 1 },
+                    nguoiLam: { $push: "$$ROOT" },
+                },
+            },
+            {
+                $unwind: "$nguoiLam",
+            },
+            {
+                $lookup: {
+                    from: "users",
+                    localField: "nguoiLam.nguoiLam",
+                    foreignField: "_id",
+                    as: "ChiTietNguoiLam",
+                },
+            },
+            {
+                $unwind: "$ChiTietNguoiLam",
+            },
+            {
+                $group: {
+                    _id: "$_id",
+                    count: { $first: "$count" },
+                    nguoiLam: {
+                        $push: {
+                            gioBuoiSang: "$nguoiLam.gioBuoiSang",
+                            gioBuoiChieu: "$nguoiLam.gioBuoiChieu",
+                            gioLamThem: "$nguoiLam.gioLamThem",
+                            fullName: "$ChiTietNguoiLam.fullName",
+                        },
+                    },
+                },
+            },
+        ]).exec();
+
+        console.log(result);
+        // Data transformation
+        const transformedData = result.map((item) => {
+            const nguoiLam = item.nguoiLam.map((person) => ({
+                fullName: person.fullName,
+                morning: person.gioBuoiSang,
+                afternoon: person.gioBuoiChieu,
+                bonus: person.gioLamThem,
+            }));
             return {
-                start: moment(event.ngayLam).format(MOMENT_FORMAT),
-                end: moment(event.ngayLam).format(MOMENT_FORMAT),
-                nguoiLam: event.nguoiLam?.fullName || event.nguoiLam.username,
-                ...rest,
+                _id: item._id,
+                count: item.count,
+                nguoiLam: nguoiLam,
             };
         });
 
-        // all day in month
-        // Get the current month
-        const daysInMonth = moment().daysInMonth();
-        // Create an array of objects for each day in the current month
-        const arrayOfObjects = Array.from({ length: daysInMonth }, (_, index) => {
-            const currentDay = index + 1;
-            const date = moment().clone().date(currentDay);
-
-            return {
-                date: date.format('YYYY-MM-DD'),
-                text: date.format('DD'),
-            };
+        // Create table structure
+        const table = {};
+        transformedData.forEach((item) => {
+            item.nguoiLam.forEach((person) => {
+                if (!table[person.fullName]) {
+                    table[person.fullName] = {
+                        morning: Array(31).fill(0),
+                        afternoon: Array(31).fill(0),
+                        bonus: Array(31).fill(0),
+                    };
+                }
+                table[person.fullName].morning[item._id - 1] += person.morning;
+                table[person.fullName].afternoon[item._id - 1] += person.afternoon;
+                table[person.fullName].bonus[item._id - 1] += person.bonus;
+            });
         });
 
-        console.log(arrayOfObjects);
-        console.log(`🚀 🚀 file: cham-cong.js:139 🚀 router.get 🚀 formattedEvents`, formattedEvents);
+        // Convert the table to an array of arrays (AOA)
+        const aoa = [];
 
-        var ws = XLSX.utils.json_to_sheet(formattedEvents);
-        var wb = XLSX.utils.book_new(); XLSX.utils.book_append_sheet(wb, ws, "Data");
-        var buf = XLSX.write(wb, { type: "buffer", bookType: "xlsx" });
+        // Header row
+        const headerRow = ["nguoiLam", "Shift", ...Array.from({ length: 31 }, (_, i) => i + 1)];
+        aoa.push(headerRow);
+
+        // Data rows
+        Object.entries(table).forEach(([fullName, values]) => {
+            aoa.push([fullName, "morning", ...values.morning]);
+            aoa.push([fullName, "afternoon", ...values.afternoon]);
+            aoa.push([fullName, "bonus", ...values.bonus]);
+        });
+
+        // Create Excel workbook and sheet
+        const ws = XLSX.utils.aoa_to_sheet(aoa);
+        const wb = XLSX.utils.book_new();
+        XLSX.utils.book_append_sheet(wb, ws, "Table");
+
+        // Send the Excel file as a response
+        const buf = XLSX.write(wb, { type: "buffer", bookType: "xlsx" });
         res.attachment("SheetJSExpress.xlsx");
         res.status(200).end(buf);
     } catch (error) {
         console.error(error);
-        res.status(500).send('Internal Server Error');
+        res.json({
+            message: error.message,
+        });
     }
 });
 
-router.get('/bang-cham-cong', async (req, res) => {
+router.get("/bang-cham-cong", async (req, res) => {
     try {
         const ngayLam = await NgayLam.find();
 
-        return res.status(200).render('bang-cham-cong', {
-            ngayLam
+        return res.status(200).render("bang-cham-cong", {
+            ngayLam,
         });
     } catch (error) {
-        console.log('error:115 ', error);
-        return res.redirect('/');
+        console.log("error:115 ", error);
+        return res.redirect("/");
     }
 });
 
