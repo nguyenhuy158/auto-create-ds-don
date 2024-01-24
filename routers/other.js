fa0da2ed776218d6b39f9a37416b4d485eb37015
huy.ntq02 <huy.ntq02@gmail.com>
2024-01-24 09:36:58 +0700

    Add new files and update routes and views

--- routers/other.js
+++ routers/other.js
@@ -5,17 +5,17 @@ router.get("/huong-dan-su-dung", (req, res) => {
     res.render("tutorial");
 });
 
-router.get("/about", (req, res) => {
-    res.render("about");
+router.get("/gioi-thieu", (req, res) => {
+    res.render("gioi-thieu");
 });
 
 router.get("/lich-su", async (req, res) => {
     let history = await readArrayFromFile();
-    res.render("history", { history });
+    res.render("lich-su", { history });
 });
 
-router.get("/version", async (req, res) => {
-    res.render("version");
+router.get("/phien-ban", async (req, res) => {
+    res.render("phien-ban");
 });
 
 module.exports = router;
